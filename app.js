98d92c55afc0a49032752064ed0e04f7cddc77b5
Oluwole <oluwoleafolabi0@gmail.com>
2025-04-09 16:41:06 +0100

    TEMP: remove rate limit

--- app.js
+++ app.js
@@ -37,13 +37,13 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(compression());
 
-const limiter = rateLimit({
-    max: 100,
-    windowMs: 60 * 60 * 1000,
-    message: 'Too many requests from this IP, please try again in an hour'
-});
+// const limiter = rateLimit({
+//     max: 100,
+//     windowMs: 60 * 60 * 1000,
+//     message: 'Too many requests from this IP, please try again in an hour'
+// });
 
-app.use('/api', limiter);
+// app.use('/api', limiter);
 
 // Routes
 app.use('/api/auth', authRoutes);
